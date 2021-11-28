e31f9f723d091078f4d70aaeeeeb68c10146dd21
Higor <higorpiresoliveira@gmail.com>
2021-11-28 19:20:55 -0300

    remove comment

--- main.py
+++ main.py
@@ -2,6 +2,3 @@ from controller.ControladorSistema import ControladorSistema
 
 if __name__ == "__main__":
     ControladorSistema().inicializa_sistema()
-
-# TODO: Lembrar de remover itens necessários das mensagens
-# TODO: Usar as mensagens do messages
