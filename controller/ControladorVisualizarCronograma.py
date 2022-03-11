42153c64ad219489b6a8b65bdcf3c2a4bd295ec2
Higor <higorpiresoliveira@gmail.com>
2022-03-11 17:12:40 -0300

    Fix data load

--- controller/ControladorVisualizarCronograma.py
+++ controller/ControladorVisualizarCronograma.py
@@ -12,8 +12,6 @@ class ControladorVisualizarCronograma:
     def __init__(self, controlador_sistema):
         self.__controlador_sistema = controlador_sistema
         self.__tela = TelaVisualizarCronograma(self)
-        self.__atividade_dao = AtividadeDAO()
-        self.__cronograma_dao = CronogramaDAO()
 
     def __new__(cls, _):
         if ControladorVisualizarCronograma.__instance is None:
@@ -22,16 +20,17 @@ class ControladorVisualizarCronograma:
 
     @property
     def atividade_dao(self) -> AtividadeDAO:
-        return self.__atividade_dao
+        return self.__controlador_sistema.controlador_atividade.dao
 
     @property
     def cronograma_dao(self) -> CronogramaDAO:
-        return self.__cronograma_dao
+        return self.__controlador_sistema.controlador_atividade.cronograma_dao
 
     def abre_tela(self):
         while True:
             event, values = self.__tela.abrir_tela(
-                self.map_atividades_to_dict())
+                self.map_atividades_to_dict()
+            )
             if event == 'exited':
                 break
             elif 'criar_' in event:
@@ -64,35 +63,18 @@ class ControladorVisualizarCronograma:
             mes = f'0{data[1]}' if data[1] < 10 else f'{data[1]}'
             lista_dias_da_semana_formatados.append(f'{dia}/{mes}')
 
-        self.__cronograma_dao.load_data()
-        print('fez as paradas')
+        lista_todas_atividades = self.atividade_dao.get_all()
 
         for i in range(7):
-            lista_id_atividades_do_dia = self.__cronograma_dao.get_atividades_na_data(
-                semana_atual[i][0], semana_atual[i][1], semana_atual[i][2])
-
-            for id_atividade_na_data in lista_id_atividades_do_dia:
-                atividade = self.__atividade_dao.get(
-                    UUID(id_atividade_na_data))
-                if atividade != None:
-                    print(f"\nAtividades na data: ${i}")
-                    print(f"{atividade.nome}")
-
-            lista_todas_atividades = self.__atividade_dao.get_all()
-
-            if i == 1:
-                for atividade_todas in lista_todas_atividades:
-                    if atividade_todas != None:
-                        print("\nTodas as atividades:")
-                        print(f"{atividade_todas.nome}")
+            lista_id_atividades_do_dia = self.cronograma_dao.get_atividades_na_data(
+                semana_atual[i][0], semana_atual[i][1], semana_atual[i][2]
+            )
 
             lista_obj_atividades = []
 
             for atividade in lista_todas_atividades:
                 if str(atividade.id) in lista_id_atividades_do_dia and atividade.concluidaEm == None:
                     lista_obj_atividades.append(atividade)
-                    print("entrou")
-                    print(atividade.nome)
 
             dict_atividades_da_semana[i] = list(
                 map(lambda atividade: {
@@ -101,7 +83,8 @@ class ControladorVisualizarCronograma:
                     'tag': atividade.tag.nome,
                     'id': str(atividade.id),
                     'tempo': lista_id_atividades_do_dia.count(str(atividade.id))
-                }, lista_obj_atividades))
+                }, lista_obj_atividades)
+            )
 
         dict_data = {
             'dias_semana': lista_dias_da_semana_formatados,
@@ -121,15 +104,15 @@ class ControladorVisualizarCronograma:
         return NotImplementedError
 
     def concluir(self, codigo_atividade: str):
-        atividade = self.__atividade_dao.get(UUID(codigo_atividade))
+        atividade = self.atividade_dao.get(UUID(codigo_atividade))
         self.__controlador_sistema.controlador_concluir_atividade.abre_tela(
             atividade)
 
     def excluir(self, codigo_atividade: str):
         try:
-            atividade = self.__atividade_dao.get(UUID(codigo_atividade))
-            self.__cronograma_dao.deleta_alocacao_atividade(atividade)
-            self.__atividade_dao.remove(atividade)
+            atividade = self.atividade_dao.get(UUID(codigo_atividade))
+            self.cronograma_dao.deleta_alocacao_atividade(atividade)
+            self.atividade_dao.remove(atividade)
         except Exception:
             self.__controlador_sistema\
                 .mensagem_sistema.error(mensagens_atividade.get('erro_excluir'))
