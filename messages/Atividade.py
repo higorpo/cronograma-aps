50e4d601e622f34953bd77a6e047ddb4b20927e2
Higor <higorpiresoliveira@gmail.com>
2022-03-13 09:50:11 -0300

    Altera label

--- messages/Atividade.py
+++ messages/Atividade.py
@@ -7,5 +7,5 @@ mensagens_atividade = {
     'ja_cadastrado': 'Já existe uma atividade cadastrada com o mesmo nome!',
     'erro_excluir': 'Erro ao excluir essa atividade...',
     'label_conclusao_checkbox': 'Atividade concluída',
-    'concluir': lambda nome_atividade: f'Deseja concluir a atividade "{nome_atividade}"?'
+    'concluir': lambda nome_atividade: f'Deseja concluir a atividade "{nome_atividade}"?\nAo concluir essa atividade, todos os blocos\nde alocação serão concluídos!'
 }
